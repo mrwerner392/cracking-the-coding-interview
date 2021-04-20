8670a29e99b53ea54989b986b504ee4fe01f1bc3
mrwerner392 <mrwerner392@gmail.com>
2021-04-20 14:28:01 -0400

    1.1 files.

---
+++ chapter1/one/index.js
@@ -0,0 +1,12 @@
+const unique = str => {
+
+}
+
+const uniqueNoStructs = str => {
+
+}
+
+export default {
+  unique,
+  uniqueNoStructs
+}
---
+++ chapter1/one/test.js
@@ -0,0 +1,9 @@
+import { unique, uniqueNoStructs } from './index';
+
+test('unique function is defined', () => {
+  expect(unique).toBeDefined()
+})
+
+test('uniqueNoStructs function is defined', () => {
+  expect(uniqueNoStructs).toBeDefined()
+})
